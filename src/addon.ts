6e1155c4c4241352c993d9e23f9a75eb21bea9cb
Quinn McHugh <qpmchugh@gmail.com>
2024-06-14 00:05:10 -0400

    Add Google search for collections and item URLs

--- src/addon.ts
+++ src/addon.ts
@@ -2,6 +2,7 @@ import { ColumnOptions } from "zotero-plugin-toolkit/dist/helpers/virtualizedTab
 import { DialogHelper } from "zotero-plugin-toolkit/dist/helpers/dialog";
 import hooks from "./hooks";
 import { createZToolkit } from "./utils/ztoolkit";
+import { config } from "../package.json";
 
 class Addon {
   public data: {
@@ -32,6 +33,102 @@ class Addon {
     };
     this.hooks = hooks;
     this.api = {};
+
+    this.initializeCollectionMenuItem();
+    this.initializeItemMenuItem();
+  }
+
+  private initializeCollectionMenuItem() {
+    const { ztoolkit } = this.data;
+
+    ztoolkit.Menu.register("collection", {
+      tag: "menuitem",
+      id: "zgi-good-search-collection-command",
+      label: "Google It - Launch Google search from collection URLs",
+      icon: `chrome://${config.addonRef}/content/icons/google-icon.png`,
+      commandListener: async () => {
+        const selectedCollection = ZoteroPane.getSelectedCollection();
+        if (selectedCollection) {
+          const items = await selectedCollection.getChildItems();
+          const urls = items
+            .map((item) => item.getField("url"))
+            .filter((url) => url);
+
+          if (urls.length) {
+            const googleSearchQuery = this.buildGoogleSearchQuery(urls);
+            const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(googleSearchQuery)}&focus=searchbar`;
+            Zotero.launchURL(googleSearchUrl);
+          } else {
+            Zotero.alert(
+              null,
+              "No URLs found",
+              "No URLs found in the selected collection.",
+            );
+          }
+        } else {
+          Zotero.alert(
+            null,
+            "No collection selected",
+            "Please select a collection to perform a Google search.",
+          );
+        }
+      },
+    });
+  }
+
+  private initializeItemMenuItem() {
+    const { ztoolkit } = this.data;
+
+    ztoolkit.Menu.register("item", {
+      tag: "menuitem",
+      id: "zgi-good-search-item-command",
+      label: "Google It - Launch Google search from item URLs",
+      icon: `chrome://${config.addonRef}/content/icons/google-icon.png`,
+      commandListener: async () => {
+        const selectedItems = ZoteroPane.getSelectedItems();
+        if (selectedItems) {
+          const urls = selectedItems
+            .map((item) => item.getField("url"))
+            .filter((url) => url);
+
+          if (urls.length) {
+            const googleSearchQuery = this.buildGoogleSearchQuery(urls);
+            const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(googleSearchQuery)}&focus=searchbar`;
+            Zotero.launchURL(googleSearchUrl);
+          } else {
+            Zotero.alert(
+              null,
+              "No URLs found",
+              "No URLs found in the selected items.",
+            );
+          }
+        } else {
+          Zotero.alert(
+            null,
+            "No item(s) selected",
+            "Please select one or more items to perform a Google search.",
+          );
+        }
+      },
+    });
+  }
+
+  /**
+   * Clean the URL by stripping "http://", "https://", or "www.".
+   * @param url URL to clean.
+   * @returns Cleaned URL.
+   */
+  private cleanUrl(url: string): string {
+    return url.replace(/^(https?:\/\/)?(www\.)?/, "");
+  }
+
+  /**
+   * Construct a Google search query from the provided URLs.
+   * @param urls Array of URLs to search.
+   * @returns Google search query string.
+   */
+  private buildGoogleSearchQuery(urls: string[]): string {
+    return urls.map((url) => `site:${this.cleanUrl(url)}`).join(" | ");
   }
 }
 
